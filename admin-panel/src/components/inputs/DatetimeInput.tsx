4b3f20afe0cd540ef61583dc38c56c140414943b
code matters <46483685+tnebcyber@users.noreply.github.com>
2025-03-19 14:28:13 -0400

    publish time use local Datetime

--- admin-panel/src/components/inputs/DatetimeInput.tsx
+++ admin-panel/src/components/inputs/DatetimeInput.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {Calendar} from "primereact/calendar";
 import {InputPanel} from "./InputPanel";
+import { toDatetime, toZonelessStr } from "../formatter";
 
 export function DatetimeInput(
     props: {
+        showTime:boolean,
         data: any,
         column: { field: string, header: string },
         register: any
@@ -13,25 +15,22 @@ export function DatetimeInput(
         inline:boolean
     }) {
     return <InputPanel  {...props} childComponent={(field: any) => {
-        let d = null
-        
-        if (field.value) {
-            d= new Date(field.value);
-        }
+        const d = field.value && typeof(field.value) === "string" 
+            ? toDatetime(field.value) 
+            : field.value;
         
         return <Calendar
             inline={props.inline}
             id={field.name}
-            showTime
+            showTime ={props.showTime}
             hourFormat="24"
             value={d}
             className={'w-full'}
             readOnlyInput={false}
             onChange={
                 e => {
-                    if (e.value) {
-                        field.onChange(e.value)
-                    }
+                    //remove zone info, let backend save date as it is
+                    field.onChange(toZonelessStr(e.value));
                 }
             }/>
     }}/>
